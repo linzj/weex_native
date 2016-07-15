84f2c0225bbb995d0bd28d3ba88be1e4aca462ad
linzj <zuojian.lzj@alibaba-inc.com>
2016-07-15 10:41:51 +0800

    update build script.

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 export TARGET_ARCH=armeabi-v7a
 cd v8 && ./build.sh
+cd ../ && mkdir -p aliout/armeabi-v7a
+cd aliout/armeabi-v7a/ && arm-linux-androideabi-strip ../../v8/out/android_arm.release/obj.target/alisrc/libweexcore.so -o libweexcore.so
+arm-linux-androideabi-strip ../../v8/out/android_arm.release/buildcache -o buildcache
